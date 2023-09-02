1f840637f3247fa7db8594cb79b75bd4e76f86e9
bibekgyawali2 <gyawali.bibek02@gmail.com>
2023-09-02 23:07:48 +0545

    dart fix order fetch

--- lib/data/datasources/fetchorder/fetchorder.dart
+++ lib/data/datasources/fetchorder/fetchorder.dart
@@ -1,19 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../../constants/firebaseconstants.dart';
 import '../../models/ordermodal.dart';
-import '../../models/productmodel.dart';
 
 class FetchOrder {
   Future<List<OrderModal>> fetchData() async {
     try {
       QuerySnapshot snapshot = await firestore.collection('Order').get();
       List<DocumentSnapshot> documents = snapshot.docs;
-      List<OrderModal> productList = documents.map((DocumentSnapshot document) {
+
+      List<OrderModal> orderList = documents.map((DocumentSnapshot document) {
         Map<String, dynamic> data = document.data() as Map<String, dynamic>;
         return OrderModal.fromJson(data);
       }).toList();
-      return productList;
+      return orderList;
     } catch (e) {
+      print(e);
       rethrow;
     }
   }
